9221bad68755300b2cc4d86a8c89b78ae27b02db
Björn Ramberg <bjoern.ramberg@gmail.com>
2026-08-16 21:51:54 +0200

    Add --version/-v flag to CLI

--- zenplayer/__main__.py
+++ zenplayer/__main__.py
@@ -1,12 +1,19 @@
+import argparse
 import faulthandler
 import os
 import time
 
-from zenplayer import diagnostics, nonblocking_output
+from zenplayer import __version__, diagnostics, nonblocking_output
 from zenplayer.app import ZenPlayer
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(description="Terminal YouTube Music client")
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    parser.parse_args()
+
     stack_log = open("/tmp/zenplayer-stack.log", "w")
     faulthandler.dump_traceback_later(10, repeat=True, file=stack_log)
     nonblocking_output.install()
